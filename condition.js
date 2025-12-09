900c910f795cf8e54cc424b76a233156ed7d9bcc
thevie <fougerouxjassem83@gmail.com>
2025-12-09 16:07:07 +0300

    decouverte des conditions

---
+++ condition.js
@@ -0,0 +1,66 @@
+
+    //**les conditions */
+
+
+let nombreFruit = 19;
+
+
+        // ma condition commence par if **/
+        //*if(ma condition ){action a éxecuter}
+
+if (nombreFruit == 18 ) {    //c'est une comparaison**/
+
+    alert("vous avez atteint 18 fruits");   //**fonction alert() */
+
+};
+
+//**j'affiche a l'alerte si le nombre est égale à 18 */
+
+
+let noteEleve = 10;
+
+if(noteEleve >= 10) {
+alert("vous etes admis avec une note de" + noteEleve)
+
+};
+
+
+
+
+let ageNetflix = 18; 
+
+if(ageNetflix >= 18) {
+alert("vous avez plus de " + ageNetflix +  "ans regardez rambo")
+
+};
+    
+
+
+let ageNetflix1 = -18;
+if("regarder le roi lion " + ageNetflix1){
+    alert("le film rambo est inaccesible" +  ageNetflix1 + "ans")
+
+
+};
+
+
+
+
+let ageTelespectateur = 18;
+    if(ageTelespectateur < 18){
+        alert("vous pouvez regarder le roi lion! car vous avez" + ageTelespectateur);
+
+    };
+
+
+//la condition if*
+
+let pointPermis = 12;
+if(pointPermis==12){
+alert("vous avez un bonus!");
+
+//else ça veut dire sinon
+} else{
+    alert("vous avez un bonus!");
+
+}
