214b6d28dede48973e6aebb3b7c87db160ba228e
thevie <fougerouxjassem83@gmail.com>
2025-12-09 14:08:22 +0300

    ajout des modifications

--- operateur.js
+++ operateur.js
@@ -1,21 +1,73 @@
-console.log("ici je code les operateurs en javascript");
+console.log("ici c'est l'operateur");
+// calcul addition 
+let valeurAjoutee = 7;
+let prixUnitaire = 3;   // ici ma variable c'est 3
 
-//calcule addition 
+let prixAdditionne1 = prixUnitaire + valeurAjoutee; // j'utilise les variables
+console.log("prixAdditionne1 :", prixAdditionne1); // j'affiche le résultat
 
-let prixunitaire = 3;   //ici ma variable c'est 3
-let prixAdditionne1 = 3+7;    //ici j'additionne des chiffre 
-console.log("prixAdditionne1 : ", prixAdditionne1);    //ici j'affiche le résultat de mon calcul
+let valeurReduction = 5;
 
-
-//ici je vais calculzer a l'aide de variable
-
-let valeurajoutee = 7 ;
-
-console.log("prix de la valeur ajoutee : ", valeurajoutee + prixAdditionne1);
-
+// ici je vais calculer à l'aide de variable
+console.log("prix de la valeur ajoutée :", valeurAjoutee + prixAdditionne1);
 
 let prixAdditionne2 = 9;
 
-//  jai fais un autre exemple de calcule 
-console.log("valeur ajouter : ", prixAdditionne2 + valeurajoutee);
+// autre exemple de calcul 
+console.log("valeur ajoutée :", prixAdditionne2 + valeurAjoutee);
+
+let eleve1 = "fatima";
+let eleve2 = "amina";
+
+// concatenation 
+console.log(eleve1 + eleve2);
+
+let elevecombo = eleve1 + " " + eleve2; // espace entre les deux
+console.log(elevecombo);
+
+// soustraction 
+let prixReduit = prixUnitaire - valeurReduction;
+console.log("résultat prix réduit :", prixReduit);
+
+// multiplication
+let prixMultiplie = prixUnitaire * valeurAjoutee;
+console.log("résultat du prixMultiplie :", prixMultiplie);
+
+// division
+let prixDivise = prixUnitaire / valeurAjoutee; // attention au nom de variable
+console.log("résultat prixDivise :", prixDivise);
+
+// opérateur de comparaison
+let village1 = "Mamoudzou";
+let commune1 = "Mamoudzou";
+
+console.log(village1, commune1);
+
+// vérifier si les deux ont la même valeur
+console.log(village1 === commune1);  // tu avais mis village au lieu de village1
+
+// signes supérieur et inférieur
+let age1 = 12;
+let distanceparcourue = 5;
+console.log(age1 > distanceparcourue);
+
+
+
+//ici ya les exemple des signes, poour bien comprendre les signes
+
+
+//esque l'age est inferieur ou égale a la distance?
+console.log( age1<= distanceparcourue);
+
+
+//esque le village est differant de la commune?
+console.log(village1 != commune1);
+
+
+//esque la commune est strictement different du village?
+console.log(village1 !== commune1);
+
+
+
+//
 
